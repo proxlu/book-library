f4eb32a14710e8e5359a5d462fc7d7d9f521b996
Eduardo Solano <105125779+proxlu@users.noreply.github.com>
2024-12-23 22:48:38 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,23 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String ISBN;
+
+    public Livro(String titulo, String autor, String ISBN) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+}
